92e3cb9bb29b16e8aa0ef0aa3370800a9cb99c9f
MarcosOli <marcos.xprox@gmail.com>
2025-05-05 17:31:42 -0300

    add rota raiz

--- todolist_nivel_8/main.py
+++ todolist_nivel_8/main.py
@@ -4,3 +4,7 @@ from .routes import router
 app = FastAPI()
 app.include_router(router, prefix="/api")
 
+@app.get("/")
+def root():
+    return {"message": "API da To-Do List no ar! Vá para /api/docs"}
+
