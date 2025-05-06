b6fd15a9e7e1c9e571049880f7475366bba56566
MarcosOli <marcos.xprox@gmail.com>
2025-05-05 22:00:48 -0300

    correção de rotas

--- todolist_nivel_8/main.py
+++ todolist_nivel_8/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from .routes import router
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 app.include_router(router, prefix="/api")
+
+@app.get("/")
+def root():
+    return RedirectResponse(url="/docs")
